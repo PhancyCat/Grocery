4850e5f668964a05cb03bf3f4985c0d9984895e4
Garrett Summers <gsummers@calpoly.edu>
2015-03-15 00:44:12 -0700

    Form validation for the main page and the add item page

--- app/src/main/java/shopstop/grocerylist/AddItem.java
+++ app/src/main/java/shopstop/grocerylist/AddItem.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.parse.ParseObject;
 
@@ -22,32 +26,28 @@ import shopstop.grocerylist.tasks.AddPriceTask;
 
 public class AddItem extends Activity {
 
+    private EditText mItem;
+    private EditText mPrice;
+    private EditText mQuant;
+    private EditText mStore;
+    private EditText mAddress;
+    private Button mAddButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
         setTitle("Add Price");
 
-        final ParseObjectHandler handler = new ParseObjectHandler() {
-            @Override
-            public void onCallComplete(ParseObject parseObject) {
-                finish();
-            }
-        };
+        mItem = (EditText)findViewById(R.id.itemEdit);
+        mPrice = (EditText)findViewById(R.id.priceEdit);
+        mQuant = (EditText)findViewById(R.id.quantEdit);
+        mStore = (EditText)findViewById(R.id.storeEdit);
+        mAddress = (EditText)findViewById(R.id.addressEdit);
+        mAddButton = (Button) findViewById(R.id.addButton);
 
-        Button searchButton = (Button) findViewById(R.id.addButton);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        setListeners();
 
-                ParseStore store = new ParseStore("Trader Joe's", "3977 S Higuera St, San Luis Obispo, CA 93401");
-                ParseItem item = new ParseItem("Juice", "oz", "12.0");
-                ParsePrice price = new ParsePrice("3.99", false, item, store);
-
-                // Add the price
-                AddPriceTask task = new AddPriceTask(handler, price);
-                task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-            }
-        });
     }
 
 
@@ -87,5 +87,173 @@ public class AddItem extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_add_item, container, false);
             return rootView;
         }
+    }
+
+    private void setListeners() {
+        mItem.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mItem.getText().toString();
+
+                if(mItem.getText().toString().equals("")) {
+                    mItem.setError("Input is required!");
+                    mItem.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("[a-zA-Z0-9 ]++")) {
+                    mItem.setError("Only alphanumeric characters are allowed!");
+                    mItem.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mItem.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mItem.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mPrice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mPrice.getText().toString();
+
+                if(mPrice.getText().toString().equals("")) {
+                    mPrice.setError("Input is required!");
+                    mPrice.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("\\d+[.]?\\d*")) {
+                    mPrice.setError("Only numbers are allowed!");
+                    mPrice.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mPrice.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mPrice.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mQuant.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mQuant.getText().toString();
+
+                if(mQuant.getText().toString().equals("")) {
+                    mQuant.setError("Input is required!");
+                    mQuant.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("\\d+")) {
+                    mQuant.setError("Only whole numbers are allowed!");
+                    mQuant.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mQuant.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mQuant.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mStore.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mStore.getText().toString();
+
+                if(mStore.getText().toString().equals("")) {
+                    mStore.setError("Input is required!");
+                    mStore.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("[a-zA-Z0-9 ]++")) {
+                    mStore.setError("Only alphanumeric characters are allowed!");
+                    mStore.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mStore.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mStore.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mAddress.getText().toString();
+
+                if(mAddress.getText().toString().equals("")) {
+                    mAddress.setError("Input is required!");
+                    mAddress.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("[a-zA-Z0-9 ]++")) {
+                    mAddress.setError("Only alphanumeric characters are allowed!");
+                    mAddress.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mAddress.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mAddress.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        final ParseObjectHandler handler = new ParseObjectHandler() {
+            @Override
+            public void onCallComplete(ParseObject parseObject) {
+                finish();
+            }
+        };
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mItem.getText().toString().equals("") || mItem.getError() != null ||
+                        mPrice.getText().toString().equals("") || mPrice.getError() != null ||
+                        mQuant.getText().toString().equals("") || mQuant.getError() != null ||
+                        mStore.getText().toString().equals("") || mStore.getError() != null ||
+                        mAddress.getText().toString().equals("") || mAddress.getError() != null) {
+                    ;
+                } else {
+                    ParseStore store = new ParseStore("Trader Joe's", "3977 S Higuera St, San Luis Obispo, CA 93401");
+                    ParseItem item = new ParseItem("Juice", "oz", "12.0");
+                    ParsePrice price = new ParsePrice("3.99", false, item, store);
+
+                    // Add the price
+                    AddPriceTask task = new AddPriceTask(handler, price);
+                    task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/shopstop/grocerylist/MainSearch.java
+++ app/src/main/java/shopstop/grocerylist/MainSearch.java
@@ -8,6 +8,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,9 +21,12 @@ import android.widget.EditText;
 import com.google.zxing.integration.android.*;
 import com.parse.Parse;
 
-
-
 public class MainSearch extends ActionBarActivity {
+
+    private EditText mFindItem;
+    private EditText mLocation;
+    private EditText mDistance;
+    private Button  mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +36,31 @@ public class MainSearch extends ActionBarActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.show();
 
+        mFindItem = (EditText)findViewById(R.id.findItemName);
+        mLocation = (EditText)findViewById(R.id.location);
+        mDistance = (EditText)findViewById(R.id.disance);
+        mSearchButton = (Button) findViewById(R.id.searchButton);
+
         setTitle("Search");
 
         Bundle bundle = getIntent().getExtras();
         String fillItem = null;
 
+        mFindItem.setText("");
+        mLocation.setText("");
+        mDistance.setText("");
+
         if(bundle != null) {
             fillItem = bundle.getString("itemName");
-            EditText item = (EditText) findViewById(R.id.findItemName);
-            item.setText(fillItem);
+            if(fillItem != null)
+                mFindItem.setText(fillItem);
         } else {
             Log.d("Bundle Check", "Activity was passed nothing");
         }
+
+
+        setListeners();
+
 
         final Activity act = this;
 
@@ -71,21 +89,6 @@ public class MainSearch extends ActionBarActivity {
             }
         });
 
-        Button searchButton = (Button) findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), SearchResults.class);
-
-                // Set the name of the item
-                intent.putExtra("itemName", "Juice");
-                // Set the location & radius
-                intent.putExtra("latitude", 35.28304);
-                intent.putExtra("longitude", -120.65925);
-                intent.putExtra("radius", 10.0);
-
-                startActivity(intent);
-            }
-        });
     }
 
 
@@ -125,5 +128,118 @@ public class MainSearch extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main_search, container, false);
             return rootView;
         }
+    }
+
+    public void setListeners() {
+
+        //Item Validation
+        mFindItem.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mFindItem.getText().toString();
+
+                if(mFindItem.getText().toString().equals("")) {
+                    mFindItem.setError(mFindItem.getHint() + " is required!");
+                    mFindItem.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("[a-zA-Z0-9 ]++")) {
+                    mFindItem.setError("Only Alphanumeric caracters are allowed!");
+                    mFindItem.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mFindItem.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mFindItem.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //Distance Validation
+        mDistance.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mDistance.getText().toString();
+
+                if(mDistance.getText().toString().equals("")) {
+                    mDistance.setError(mFindItem.getHint() + " is required!");
+                    mDistance.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("\\d+[.]?\\d*")) {
+                    mDistance.setError("Only numbers are allowed!");
+                    mDistance.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mDistance.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mDistance.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //Location Validation
+        mLocation.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = mLocation.getText().toString();
+
+                if(mLocation.getText().toString().equals("")) {
+                    mLocation.setError(mFindItem.getHint() + " is required!");
+                    mLocation.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else if (!str.matches("[a-zA-Z0-9 ]++")) {
+                    mLocation.setError("Only alphanumeric characters are allowed!");
+                    mLocation.setBackgroundColor(getResources().getColor(R.color.transred));
+                } else  {
+                    mLocation.setBackgroundColor(getResources().getColor(R.color.transgreen));
+                    mLocation.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //Ensure Validated
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mFindItem.getText().toString().equals("") || mFindItem.getError() != null ||
+                 mDistance.getText().toString().equals("")|| mDistance.getError() != null ||
+                 mLocation.getText().toString().equals("") || mLocation.getError() != null) {
+                    ;
+                } else {
+                    Intent intent = new Intent(getApplication(), SearchResults.class);
+
+                    // Set the name of the item
+                    intent.putExtra("itemName", "Juice");
+                    // Set the location & radius
+                    intent.putExtra("latitude", 35.28304);
+                    intent.putExtra("longitude", -120.65925);
+                    intent.putExtra("radius", 10.0);
+
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
